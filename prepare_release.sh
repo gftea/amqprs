53fa9f8d1abc6161422f543a2946fcad397e5692
gftea <ckq614@gmail.com>
2022-12-05 23:34:24 +0100

    prepare release v0.1.5

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-
-#// quick local check before release
+#// regression test before release
 
 release_tag=$1
 
 Usage() { echo "missing release tag"; exit 1; }
 [ $# -ne 1 ] && Usage
 
-cargo test 
-cargo test -F tracing
-cargo test -F compliance_assert
-cargo test --all-features 
-
-cargo doc  -p amqprs --all-features --open
-cargo publish -p amqprs --all-features --dry-run
- 
-
-git commit -a -m "prepare release ${release_tag}"
-git tag -a ${release_tag} -m "${release_tag}"
-git push origin ${release_tag}
+read -p 'commit: ' ans
+if [ "$ans" = "y" ]; then
+    git commit -a -m "prepare release ${release_tag}"
+    git tag -a ${release_tag} -m "${release_tag}"
+    git log -1
+fi
+read -p 'push tag: ' ans
+if [ "$ans" = "y" ]; then
+    git push origin ${release_tag}
+fi
+read -p 'push commit: ' ans
+if [ "$ans" = "y" ]; then
+    git push origin ${release_tag}
+fi
