695e1f2951f96747bc1eb27902e3007f84bccf9b
Jacob Miller <dev@jacobmiller22.com>
2023-01-15 14:18:41 -0500

    Altered permissions for client secrets

--- start_rabbitmq.sh
+++ start_rabbitmq.sh
@@ -2,10 +2,14 @@
 
 COMMON_NAME=AMQPRS_TEST
 
-# Create directories for rabbitmq server and client
+# Create directories for rabbitmq server and client and alter permissions
 #------------------------
 mkdir -p rabbitmq_conf/server
 mkdir -p rabbitmq_conf/client
+sudo chown -R 1001:root rabbitmq_conf/server
+sudo chmod 755 rabbitmq_conf/server
+sudo chmod 400 rabbitmq_conf/server/*
+sudo chmod 444 rabbitmq_conf/client/*
 
 # generate tls cert/key
 #------------------------
@@ -36,6 +40,7 @@ sudo chown -R 1001:root rabbitmq_conf/server
 # strict permissions is mandatory for TLS cert/key files
 sudo chmod 755 rabbitmq_conf/server
 sudo chmod 400 rabbitmq_conf/server/*
+sudo chmod 444 rabbitmq_conf/client/*
 
 # start rabbitmq server
 docker-compose down
