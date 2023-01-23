d279ed6d621a4e55becc17e46603022c209e5ff0
gftea <ckq614@gmail.com>
2023-01-23 21:27:01 +0100

    improve workflow

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,38 +1,56 @@
 #!/bin/bash
 #// prepare release
 
+function check_result () {
+    ret=$?
+    if [[ $ret != 0 ]];then
+        echo "early exit due to error!"
+        exit $ret
+    fi
+}
+
 version=$(egrep '^version = "(.+)"$' -o amqprs/Cargo.toml | cut -d '"' -f2)
 
-# check 
+# check semver
 semver_regex="[0-9]+\.[0-9]+\.[0-9]+"
 if ! [[ $version =~ $semver_regex ]]; then
     echo "error, check semantic version: '$version'"
     exit 1  
 fi
 
+# dry run check
+cargo publish -p amqprs --all-features --dry-run
+check_result
+
+cargo package --list
+check_result
+
+ls -hl target/package/amqprs-${version}.crate
+check_result
+
 read -p "Are you going to release v${version}? " ans
 if [ "$ans" != "y" ]; then
     exit 0
 fi
 
-read -p 'commit? ' ans
+read -p 'make a commit? ' ans
 if [ "$ans" = "y" ]; then
     git commit -a -m "prepare release v${version}"
-    git tag -a "v${version}" -m "v${version}"
     git log -1
 fi
-read -p 'push tag? ' ans
-if [ "$ans" = "y" ]; then
-    git push origin "v${version}"
-fi
+
 read -p 'push commit? ' ans
 if [ "$ans" = "y" ]; then
     git push
 fi
 
-cargo publish -p amqprs --all-features --dry-run
-cargo package --list
-ls -hl target/package/amqprs-${version}.crate
+read -p "push tag v${version}? " ans
+if [ "$ans" = "y" ]; then
+    git tag -a "v${version}" -m "v${version}"
+    git push origin "v${version}"
+    git log -1
+fi
+
 read -p 'Want to publish to crates.io? ' ans
 if [ "$ans" = "y" ]; then
     cargo publish -p amqprs --all-features
