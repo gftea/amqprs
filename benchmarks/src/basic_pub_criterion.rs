494530a1cb65f0014a0a63e0df9c165d2783874e
gftea <ckq614@gmail.com>
2023-02-09 21:29:20 +0100

    single core, single thread

--- benchmarks/src/basic_pub_criterion.rs
+++ benchmarks/src/basic_pub_criterion.rs
@@ -327,11 +327,11 @@ mod client_lapin {
         });
     }
 }
+
 criterion_group!{
     name = basic_pub;
-    config = Criterion::default().significance_level(0.1).sample_size(500).measurement_time(Duration::from_secs(10));
+    config = Criterion::default();
     targets = client_amqprs::amqprs_basic_pub,  client_lapin::lapin_basic_pub
 }
-
 
 criterion_main!(basic_pub);
