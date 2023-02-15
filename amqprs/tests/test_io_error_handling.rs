6ecb42a7b0013d46505a3af122633757cac4d0b0
gftea <ckq614@gmail.com>
2023-02-15 11:45:56 +0100

    test multiple copies of same connection listening to network failure

--- amqprs/tests/test_io_error_handling.rs
+++ amqprs/tests/test_io_error_handling.rs
@@ -41,23 +41,50 @@ async fn test_net_io_err_handling() {
     // declare exchange
     channel.exchange_declare(args).await.unwrap();
 
-    // wait on io failure
-    // if return `true`, it means got notification due to network failure, we try to recreate connection
-    // if return `false`, the connection may already be closed, or closed later by client or server.
-    if connection.listen_network_io_failure().await {
-        loop {
-            time::sleep(Duration::from_secs(10)).await;
+    // test if copy of same connection will also get notified
+    let connection2 = connection.clone();
+    let handle2 = tokio::spawn(async move {
+        // wait on io failure
+        // if return `true`, it means got notification due to network failure, we try to recreate connection
+        // if return `false`, the connection may already be closed, or closed later by client or server.
+        if connection2.listen_network_io_failure().await {
+            loop {
+                time::sleep(Duration::from_secs(10)).await;
 
-            if let Ok(recover_conn) = Connection::open(&conn_args).await {
-                // ... do some recovery...
-                let recover_ch = recover_conn.open_channel(None).await.unwrap();
-                assert!(true == recover_ch.is_open());
-                assert!(true == recover_conn.is_open());
-                info!("recover: {}, {}", recover_conn, recover_ch);
-                break;
-            };
+                if let Ok(recover_conn) = Connection::open(&common::build_conn_args()).await {
+                    // ... do some recovery...
+                    let recover_ch = recover_conn.open_channel(None).await.unwrap();
+                    assert!(true == recover_ch.is_open());
+                    assert!(true == recover_conn.is_open());
+                    info!("Recovery: channel {}", recover_ch);
+                    break;
+                };
+            }
         }
-    };
-    // here, old connection should be closed no matter due to network failure or closed by server
-    assert!(connection.is_open() == false);
+        // here, old connection should be closed no matter due to network failure or closed by server
+        assert!(connection2.is_open() == false);        
+    });
+    let handle1 = tokio::spawn(async move {
+        // wait on io failure
+        // if return `true`, it means got notification due to network failure, we try to recreate connection
+        // if return `false`, the connection may already be closed, or closed later by client or server.
+        if connection.listen_network_io_failure().await {
+            loop {
+                time::sleep(Duration::from_secs(10)).await;
+
+                if let Ok(recover_conn) = Connection::open(&common::build_conn_args()).await {
+                    // ... do some recovery...
+                    let recover_ch = recover_conn.open_channel(None).await.unwrap();
+                    assert!(true == recover_ch.is_open());
+                    assert!(true == recover_conn.is_open());
+                    info!("Recovery: channel {}", recover_ch);
+                    break;
+                };
+            }
+        }
+        // here, old connection should be closed no matter due to network failure or closed by server
+        assert!(connection.is_open() == false);
+    });
+    handle1.await.unwrap();
+    handle2.await.unwrap();
 }
