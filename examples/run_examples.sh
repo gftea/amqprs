583ee33dc20b948a84789179dc702b1d4213f9eb
Simon Chen <ckq614@gmail.com>
2024-07-22 09:02:28 +0000

    test basic_pub_sub

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 # set -x
 
-excludes=("longlive_basic_consumer")
-export RUSTFLAGS="$RUSTFLAGS -A dead_code -A unused_variables"
+# excludes=("longlive_basic_consumer")
+# export RUSTFLAGS="$RUSTFLAGS -A dead_code -A unused_variables"
 
-function is_excluded () {
-    name=$1
-    for x in ${excludes[@]}; do
-        if [[ "$x" = "$name" ]];then
-            return 1
-        fi
-    done
-    return 0
-}
+# function is_excluded () {
+#     name=$1
+#     for x in ${excludes[@]}; do
+#         if [[ "$x" = "$name" ]];then
+#             return 1
+#         fi
+#     done
+#     return 0
+# }
 
-all_examples=$(cargo run --release --example 2>&1 | grep -E '^ ')
+# all_examples=$(cargo run --release --example 2>&1 | grep -E '^ ')
 
-for example in ${all_examples[@]}; do
-    is_excluded $example
-    res=$?
-    if [[ $res = 0 ]];then
-        cargo run --release --example $example --all-features
-    fi
-done
+# for example in ${all_examples[@]}; do
+#     is_excluded $example
+#     res=$?
+#     if [[ $res = 0 ]];then
+#         cargo run --release --example $example --all-features
+#     fi
+# done
+
+RUST_LOG=trace cargo run --release --example basic_pub_sub --all-features
